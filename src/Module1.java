00b91b8a629ec3da7a689a465a0627b939a3b615
Maledis87 <64336839+Maledis87@users.noreply.github.com>
2025-01-27 07:23:10 -0500

    Module 1 Assignment

---
+++ src/Module1.java
@@ -0,0 +1,17 @@
+public class Module1 {
+    public Module1() {
+    }
+
+    public static void main(String[] args) {
+        String firstName = "Anthony";
+        String lastName = "Williams";
+        String streetAddress = "3400 Aldred Lane";
+        String city = "New York";
+        int zipCode = 38927;
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
